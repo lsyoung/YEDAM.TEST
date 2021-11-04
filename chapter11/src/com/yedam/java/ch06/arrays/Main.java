7a361dc2bba402fdf6a8bdd1bea97270fc3af6e6
admin <admin@DESKTOP-B6AQBHQ>
2021-11-04 11:34:04 +0900

    arrays

--- chapter11/src/com/yedam/java/ch06/arrays/Main.java
+++ chapter11/src/com/yedam/java/ch06/arrays/Main.java
@@ -51,23 +51,53 @@ public class Main {
 		System.out.println();
 
 		/*
-		 * 배열 항목정렬 
-		 * -기본 타입(int short..) 또는 String 타입일 경우엔 자동으로 오름차순 정렬 
-		 * -사용자 정의 클래스 타입일 경우엔 클래스가 comparable() 구현하고 있어야 정렬이 된다.
+		 * 배열 항목정렬 -기본 타입(int short..) 또는 String 타입일 경우엔 자동으로 오름차순 정렬 -사용자 정의 클래스 타입일
+		 * 경우엔 클래스가 comparable() 구현하고 있어야 정렬이 된다.
 		 */
 		int[] scores = { 99, 97, 98 };
 		Arrays.sort(scores);
-		for(int score : scores) {
+		for (int score : scores) {
 			System.out.println(score);
 		}
-		
-		String[] names = {"홍길동","박동수","김민수"};
+
+		String[] names = { "홍길동", "박동수", "김민수" };
 		Arrays.sort(names);
-		for(String name : names) {
+		for (String name : names) {
 			System.out.println(name);
 		}
-		
+		System.out.println();
 
+		Member m1 = new Member("홍길동");
+		Member m2 = new Member("박동수");
+		Member m3 = new Member("김미수");
+
+		Member[] members = { m1, m2, m3 };
+		Arrays.sort(members);
+		for (Member member : members) {
+			System.out.println(member);
+		}
+		System.out.println();
+
+		// 배열 항목 검색: 배열항목에서 특정값이 위치한 인덱스를 얻는 것
+		// Arrays.sort()정렬 후 Arrays.binarySearch()항목 찾음
+
+		int[] scores1 = { 99, 97, 98 };
+		Arrays.sort(scores1);
+		int index = Arrays.binarySearch(scores1, 99);
+		System.out.println("찾은 인덱스: " + index);
+
+		String[] names1 = { "홍길동", "박동수", "김민수" };
+		Arrays.sort(names1);
+		index = Arrays.binarySearch(names1, "홍길동");
+		System.out.println("찾은 인덱스: " + index);
+
+		Member m4 = new Member("홍길동");
+		Member m5 = new Member("박동수");
+		Member m6 = new Member("김미수");
+		Member[] memberList = {m4, m5, m6};
+		Arrays.sort(memberList);
+		index = Arrays.binarySearch(memberList, m6);
+		System.out.println("찾은 인덱스: " + index);
 	}
 
 }

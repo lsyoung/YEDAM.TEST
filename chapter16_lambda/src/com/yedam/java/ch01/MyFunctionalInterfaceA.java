3608e8b28463b737bedfd0676cd3247cc6ddb1e6
admin <admin@DESKTOP-B6AQBHQ>
2021-11-05 17:41:09 +0900

    람다

---
+++ chapter16_lambda/src/com/yedam/java/ch01/MyFunctionalInterfaceA.java
@@ -0,0 +1,7 @@
+package com.yedam.java.ch01;
+
+@FunctionalInterface
+public interface MyFunctionalInterfaceA {
+	public void method();
+
+}
---
+++ chapter16_lambda/src/com/yedam/java/ch01/MyFunctionalInterfaceB.java
@@ -0,0 +1,7 @@
+package com.yedam.java.ch01;
+
+
+public interface MyFunctionalInterfaceB {
+	public void method(int x);
+
+}
